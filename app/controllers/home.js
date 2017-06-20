3452b54439208a4ba0139f8e7730c93b1ea93048
arunjayaprakash <arun77733@gmail.com>
2017-06-20 10:27:29 +0530

    Add files via upload
    script for table3 to be included

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,7 @@
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
-  Disease = mongoose.model('Disease');
+  SymptomDisease = mongoose.model('SymptomDisease');
 
 module.exports = function (app) {
   app.use('/', router);
@@ -17,16 +17,64 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.get('/add-disease',function(req,res){
+router.get('/add-diseasesymptom',function(req,res){
   res.render('form1',{});
 
   });
 
-router.post('/add-disease',function(req,res){
-var dis= new Disease();
-  dis.diseaseName=req.body.dName;
-  dis.diseaseId=req.body.dId;
-  dis.save(function(err){ if(err){res.render('index',{title:'Home',message:'error while adding symptom to db'});
+router.get('/identifydisease',function(req,res){
+  res.render('form2',{});
+
+  }); 
+
+/*router.post('/identifydisease',function(req,res){
+	var sd = new SymptomDisease();
+	sd.symptomId=req.body.fever? true : false;
+/*	sd.symptomId=req.body.headache? true : false;
+	sd.symptomId=req.body.loa? true : false;
+	
+	//if(sd.symptomId=req.body.fever){
+		console.log(req.body);
+		if (sd.symptomId='on'){
+			SymptomDisease
+				.find({
+					symptomId:'1'
+			})
+		
+				.select('diseaseIdList')
+				.exec(function(err,dArray){
+					if(err){
+						res.render('index', {
+						 	message: 'Failed'
+						});
+						   }		
+					else{
+						res.render('diseaseoutput',{
+							disease: dArray
+						})
+						}	
+						
+					})
+				
+
+		}
+}); 
+
+*/
+	//}
+	
+
+
+
+
+
+
+
+router.post('/add-diseasesymptom',function(req,res){
+var symdis= new SymptomDisease();
+  symdis.sdId=req.body.sdIdfrontend;
+  symdis.symptomId=req.body.sIdfrontend;	
+  symdis.save(function(err){ if(err){res.render('index',{title:'Home',message:'error while adding symptom to db'});
   }
   else{
     res.render('index',{
